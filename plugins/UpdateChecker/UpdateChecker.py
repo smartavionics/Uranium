d4b4fcba4121fda980a05dab675768ec7afc7982
Mark Burton <burtoogle@gmail.com>
2024-05-23 17:21:39 +0100

    Now fetches my latest.json rather than UM's.

--- plugins/UpdateChecker/UpdateChecker.py
+++ plugins/UpdateChecker/UpdateChecker.py
@@ -32,7 +32,7 @@ class UpdateChecker(Extension):
     The plugin is currently only usable for applications maintained by Ultimaker. But it should be relatively easy
     to change it to work for other applications.
     """
-    url = "https://software.ultimaker.com/latest.json"
+    url = "https://raw.githubusercontent.com/smartavionics/Cura/mb-master/latest.json"
 
     def __init__(self) -> None:
         super().__init__()
